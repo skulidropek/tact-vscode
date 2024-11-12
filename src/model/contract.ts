e2ca5dff6fc36d522df33b95a4f6344d29ab7b66
skulidropek <66840575+skulidropek@users.noreply.github.com>
2024-11-12 19:06:12 +0400

    Add support for @stdlib imports for syntax highlighting (Issue #51)

--- src/model/contract.ts
+++ src/model/contract.ts
@@ -1,12 +1,13 @@
 'use strict';
 import * as path from 'path';
+import * as fs from 'fs';
 import { formatPath } from '../util';
 
 export class Contract {
     public code: string;
-    // TODO: Import needs to be a class including if is local, absolutePath, module etc
     public imports: Array<string>;
     public absolutePath: string;
+
     constructor(absoulePath: string, code: string) {
         this.absolutePath = this.formatContractPath(absoulePath);
         this.code = code;
@@ -24,12 +25,21 @@ export class Contract {
     public resolveImports() {
         const importRegEx = /^\s?import\s+[^'"]*['"](.*)['"]\s*/gm;
         let foundImport = importRegEx.exec(this.code);
+
         while (foundImport != null) {
             const importPath = foundImport[1];
-
+            
             if (this.isImportLocal(importPath)) {
                 const importFullPath = this.formatContractPath(path.resolve(path.dirname(this.absolutePath), foundImport[1]));
                 this.imports.push(importFullPath);
+            } else if (importPath.startsWith('@stdlib/')) {
+                const stdlibPath = path.resolve(path.dirname(this.absolutePath), "../node_modules/@tact-lang/compiler/stdlib");
+                const contractFileName = importPath.replace('@stdlib/', '') + '.tact';
+                const contractFullPath = path.join(stdlibPath, 'libs', contractFileName);
+
+                if (fs.existsSync(contractFullPath)) {
+                    this.imports.push(contractFullPath);
+                }
             } else {
                 this.imports.push(importPath);
             }
